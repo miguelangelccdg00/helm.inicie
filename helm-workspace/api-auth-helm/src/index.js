dff24b4d700e815f74568007333a4df3c901fc20
JuanAlberto27 <juanjurado.external@institutodeinnovacion.es>
2025-03-21 13:29:32 +0100

    modificacion de las clases index y main

---
+++ helm-workspace/api-auth-helm/src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import registroRoutes from '../src/routes/registroRoutes';
+import loginRoutes from '../src/routes/loginRoutes';
+import path from 'path'; 
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/login',loginRoutes);
+app.use('/registro',registroRoutes);
+
+app.get('/', (req, res) => 
+{
+    res.json({ message: 'Bienvenido a la API de Tickets',
+        endpoints: [{ 
+            login:
+            {
+                base: '/login',
+                operations:[
+                    { method: 'POST', path: '/loginUsuario' }
+                ]                        
+            },
+            registro:
+            {
+                base: 'registro',
+                operations:[
+                    { method: 'POST', path: '/registrarUsuario' }
+                ]
+            }
+        }]
+    })
+})
--- helm-workspace/api-auth-helm/src/main.ts
+++ helm-workspace/api-auth-helm/src/main.ts
@@ -1,18 +1,12 @@
+import app from './index';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
-import express from 'express';
-import * as path from 'path';
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api-auth-helm!' });
-});
-
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+app.listen(PORT, () => 
+{
+  console.log (` Servidor corriendo en el puerto ${PORT}`);
+  console.log (`http://localhost:${PORT}`);
+}); 
