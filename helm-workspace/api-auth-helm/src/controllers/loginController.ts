f667ec2046b00e557886c9450a44046d61216080
JuanAlberto27 <juanjurado.external@institutodeinnovacion.es>
2025-03-24 10:40:47 +0100

    Modificacion del loginController

--- helm-workspace/api-auth-helm/src/controllers/loginController.ts
+++ helm-workspace/api-auth-helm/src/controllers/loginController.ts
@@ -1,26 +1,30 @@
-import {pool} from '../../../api-shared-helm/src/database/conexion.js';
+import { pool } from '../../../api-shared-helm/src/database/conexion.js';
 import { Request, Response } from 'express';
 import { usuario } from '../models/usuario.js';
 
-class loginController 
+class LoginController 
 {
-    constructor() {}
-
-    loginUsuario = async (req: Request, res: Response): Promise<void> =>
+    async loginUsuario(req: Request, res: Response): Promise<void> 
     {
         try 
         {
+            const { nombreUsuario, contraseña } = req.body;
+            const [rows]: any = await pool.promise().query('SELECT * FROM usuario WHERE nombreUsuario = ? AND contraseña = ?', [nombreUsuario, contraseña]);
 
-            const loginUsuario: usuario = req.body;
-            const [result] = await pool.promise().query('INSERT INTO usuario SET ?', [loginUsuario]);
-            res.status(202).json({message:'Usuario logueado con exito'});
-            return;
+            if (rows.length > 0) 
+            {
+                res.status(200).json({ message: 'Usuario logueado con éxito' });
+            } 
+            else 
+            {
+                res.status(401).json({ message: 'Credenciales incorrectas' });
+            }
         } 
         catch (error) 
         {
-            console.error('Ha ocurrido un error al loguear el usuario: ', error);
-            res.status(500).json({message:'Error al loguear el usuario '});
+            console.error('Error al loguear usuario: ', error);
+            res.status(500).json({ message: 'Error interno en el servidor' });
         }
     }
 }
-module.exports = new loginController();
+export default new LoginController();
