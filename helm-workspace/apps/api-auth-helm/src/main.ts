1e798028ddda850d9d352ae6ccabaa432e528c1b
miguelangelccdg00 <miguelcruz.external@institutodeinnovacion.es>
2025-03-24 17:26:18 +0100

    update

--- helm-workspace/apps/api-auth-helm/src/main.ts
+++ helm-workspace/apps/api-auth-helm/src/main.ts
@@ -1,12 +1,15 @@
 import app from './index';
 import * as dotenv from 'dotenv';
- 
-dotenv.config();
- 
-const PORT = process.env.PORT || 3000;
- 
+import * as path from 'path';
+
+
+dotenv.config({ path: path.join(process.cwd(), 'apps', 'api-shared-helm', 'claves.env') });
+
+
+const PORT = process.env.PORT || 3009;
+
 app.listen(PORT, () =>
 {
-  console.log (` Servidor corriendo en el puerto ${PORT}`);
-  console.log (`http://localhost:${PORT}`);
+  console.log(` Servidor corriendo en el puerto ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
