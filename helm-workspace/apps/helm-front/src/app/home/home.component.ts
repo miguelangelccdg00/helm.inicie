d3365b0c7e876c2364711c55ea6f1fd1dacd4e3a
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-03-24 13:31:00 +0100

    Lo que se había hecho en el anterior proyecto en cuanto a front, ha sido transferido a este proyecto.

--- helm-workspace/apps/helm-front/src/app/home/home.component.ts
+++ helm-workspace/apps/helm-front/src/app/home/home.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../login/login.component';
+import { CarruselComponent } from '../carrusel/carrusel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoginComponent, CarruselComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.sass',
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  
+  mostrarCarrusel: boolean = true;
+
+  constructor() {
+    this.checkScreenSize(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize((event.target as Window).innerWidth);
+  }
+
+  checkScreenSize(width: number) {
+    this.mostrarCarrusel = width > 500;
+  }
+}
--- helm-workspace/apps/helm-front/src/app/login/login.component.ts
+++ helm-workspace/apps/helm-front/src/app/login/login.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.sass',
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+
+export class LoginComponent {
+
+  loginForm = new FormGroup({
+    usuario: new FormControl('', Validators.required),
+    contrasena: new FormControl('', Validators.required),
+  });
+
+  usuarioPlaceholder: string = "";
+  contrasenaPlaceholder: string = "";
+  mostrarRecordar: boolean = true;
+  mostrarDescubrir: boolean = false;
+
+  constructor() {
+    this.updatePlaceholders(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const screenWidth = (event.target as Window).innerWidth;
+    this.updatePlaceholders(screenWidth);
+  }
+
+  updatePlaceholders(width: number) {
+    if (width <= 500) {
+      this.usuarioPlaceholder = "Introduce tu nombre de usuario";
+      this.contrasenaPlaceholder = "Contraseña";
+      this.mostrarRecordar = false;
+      this.mostrarDescubrir = true;
+    } else {
+      this.usuarioPlaceholder = "Introduce tu usuario";
+      this.contrasenaPlaceholder = "Introduce tu contraseña";
+      this.mostrarRecordar = true;
+      this.mostrarDescubrir = false;
+    }
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Inicio de sesión exitoso', this.loginForm.value);
+    } else {
+      console.log('El usuario o la contraseña no son válidos');
+    }
+  }
+  
+}
