004390f308619a7c90cdf644900c3a2ed4e7b247
miguelangelccdg00 <miguelcruz.external@institutodeinnovacion.es>
2025-03-25 10:22:53 +0100

    Update app.component.ts

--- helm-workspace/apps/helm-front/src/app/app.component.ts
+++ helm-workspace/apps/helm-front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; // Añade esta importación
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HttpClientModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
