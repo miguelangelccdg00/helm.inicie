3fb763bf34d6882df9cd51028b102658548984e5
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-03-27 11:48:05 +0100

    Botón que lleva a soluciones, a falta de darle estilos.

--- helm-workspace/apps/helm-front/src/app/app.routes.ts
+++ helm-workspace/apps/helm-front/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Route } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { authGuard } from './auth.guard';
+import { StoreSolucionesComponent } from './store-soluciones/store-soluciones.component';
 
 export const appRoutes: Route[] = [
     {path: '', component: HomeComponent},
-    {path: 'menu', component: MenuComponent, canActivate: [authGuard]}
+    {path: 'menu', component: MenuComponent, canActivate: [authGuard]},
+    {path: 'store-soluciones', component: StoreSolucionesComponent}
 ];
--- helm-workspace/apps/helm-front/src/app/menu/menu.component.ts
+++ helm-workspace/apps/helm-front/src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -9,6 +10,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './menu.component.sass',
 })
 export class MenuComponent {
+
+  constructor(private router: Router) {}
+
   // Propiedad para controlar el elemento seleccionado
   itemSeleccionado: number = -1;
   
@@ -22,4 +26,10 @@ export class MenuComponent {
   estaSeleccionado(index: number): boolean {
     return this.itemSeleccionado === index;
   }
+
+  // Método para llevar a store soluciones
+  irAStoreSoluciones() {
+    this.router.navigate(['store-soluciones']);
+  }
+
 }
