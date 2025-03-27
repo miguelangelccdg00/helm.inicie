25caa853faee94a2ef3497c18d4f946890838926
miguelangelccdg00 <miguelcruz.external@institutodeinnovacion.es>
2025-03-27 09:56:11 +0100

    added menu

--- helm-workspace/apps/helm-front/src/app/menu/menu.component.ts
+++ helm-workspace/apps/helm-front/src/app/menu/menu.component.ts
@@ -8,4 +8,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.sass',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  // Propiedad para controlar el elemento seleccionado
+  itemSeleccionado: number = -1;
+  
+  // Método para seleccionar un elemento del menú
+  seleccionarItem(index: number, event: Event): void {
+    event.preventDefault();
+    this.itemSeleccionado = index;
+  }
+  
+  // Método para verificar si un elemento está seleccionado
+  estaSeleccionado(index: number): boolean {
+    return this.itemSeleccionado === index;
+  }
+}
