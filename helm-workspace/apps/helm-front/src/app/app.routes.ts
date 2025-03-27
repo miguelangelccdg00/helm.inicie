c6711dff968418b78b30eef088ef6abdd3c3324f
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-03-27 17:15:49 +0100

    Evento que al clickar una solución, te lleve a su modificación (en progreso...).

--- helm-workspace/apps/helm-front/src/app/app.routes.ts
+++ helm-workspace/apps/helm-front/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { authGuard } from './auth.guard';
 import { StoreSolucionesComponent } from './store-soluciones/store-soluciones.component';
+import { ModificarSolucionComponent } from './modificar-solucion/modificar-solucion.component';
 
 export const appRoutes: Route[] = [
     {path: '', component: HomeComponent},
     {path: 'menu', component: MenuComponent, canActivate: [authGuard]},
-    {path: 'store-soluciones', component: StoreSolucionesComponent}
+    {path: 'store-soluciones', component: StoreSolucionesComponent},
+    {path: 'modificar-solucion/:id', component: ModificarSolucionComponent}
 ];
