2317e845a1d73b69e3f9f1b532a33a74711b0559
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-04-30 17:34:51 +0200

    Servicios creados: getStoreSectores, deleteStoreSector y createStoreSector.

---
+++ helm-workspace/apps/helm-front/src/app/services/sectores.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { StoreSectores, DeleteSectorResponse } from '@modelos-shared/storeSectores';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SectoresService {
+
+  private sectoresUrl = 'http://localhost:3009/storeSectores';
+
+  constructor(private https: HttpClient) { }
+  
+    getStoreSectores(): Observable<StoreSectores[]> {
+      const url = `${this.sectoresUrl}/listCompleteSectores`;
+      const headers = new HttpHeaders().set('Content-Type', 'application/json');
+      return this.https.get<StoreSectores[]>(url, { headers });
+    }
+  
+    deleteStoreSector(idSector: number, idSolucion: number): Observable<DeleteSectorResponse> {
+      const url = `${this.sectoresUrl}/deleteSectores/${idSolucion}/${idSector}`;
+      const headers = new HttpHeaders().set('Content-Type', 'application/json');
+      return this.https.delete<DeleteSectorResponse>(url, { headers });
+    }
+  
+    createStoreSector(sector: StoreSectores): Observable<StoreSectores> {
+      const url = `${this.sectoresUrl}/createStoreSectores`;
+      const headers = new HttpHeaders().set('Content-Type', 'application/json');
+      return this.https.post<StoreSectores>(url, sector, { headers });
+    }
+}
