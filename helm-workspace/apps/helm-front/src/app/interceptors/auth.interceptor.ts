17951bf20a47c88773194dd89f66b1322c520005
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-05-09 14:47:19 +0200

    Interceptor creado para recibir el error 403: auth.interceptor / En progreso de funcionar...

---
+++ helm-workspace/apps/helm-front/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('token');
+
+    let authReq = req;
+    if (token) {
+      authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401 || error.status === 403) {
+          console.warn('Interceptor detectó error 403/401, redirigiendo a login...');
+          console.log('Token inválido o expirado:', token);
+          localStorage.clear();
+          this.router.navigate(['/']);
+        }
+        return throwError(() => error);
+      })
+    );
+  }
+}
